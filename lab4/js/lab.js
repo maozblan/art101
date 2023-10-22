a3b8c3e35b3b06bd8c14c6ee89e663d64d6cb3f1
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-10-22 14:49:50 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * pseudocde for rock paper scissors. (use period)
+ *
+ * @link   https://maozblan.github.io/art101/lab4/index.html
+ * @file   This file has only pseudocode.
+ * @author Lyssa.
+ * @since  22.10.2023
+ */
 
-// Constants
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// find a friend (over the internet)
+	// make buttons for “rock”, “paper”, and “scissors”
+	// have countdown 
+		// (start loop) start the game :D
+			// begin countdown for choosing one button
+			// each player picks a button but their choice is hidden from the other player
+			// button selection is locked once countdown ends
+			// print “rock”, then “paper”, then “scissors” on the screen
+			// print “shoot” on the screen
+			// print both player’s pick onto the screen
+			// check for winner and loser
+			// winner = ROCK when rock and scissor, SCISSOR when scissor and paper, PAPER when paper and rock
+			// loser = ROCK when rock and paper, SCISSOR when scissor and rock, PAPER when paper and scissors
+			// if both players pick the same thing, loop! 
+	// congratulate the winner
