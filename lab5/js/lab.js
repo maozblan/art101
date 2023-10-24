f74fb28ff11ed7a5417a8e98e505e397de38798f
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-10-24 09:56:10 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * a couple of variables to print.
+ *
+ * @link   https://maozblan.github.io/art101/lab5/index.html
+ * @file   This file has a bit of variables that print things.
+ * @author Lyssa.
+ * @since  27.10.2023
+ */
+
 
 // Constants
+const make = ['acura', 'ford', 'toyota', 'bmw', 'tesla'];
+const ran = ['wizards', 'unicorns', 'dragons'];
+const color = ['graphite', 'moonlight', 'sundew', 'mist'];
+const year = [2019, 2021, 1982, 1543, 2026, 1996];
+const ownIt = [true, false];
 
 // Functions
+// random number calcuation from chatGPT: Math.floor(Math.random() * (max - min + 1)) + min;
+let num = Math.floor(Math.random() * (1000 + 1));
+let age = 2023 - year[num % year.length];
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+document.writeln("Hypothetically there is a car from " + make[num % make.length] + " made by " + ran[num % ran.length] + ".<br>");
+document.writeln("The car has the paint color of " + color[num % color.length] + " and is " + age + " years old.<br>");
+if (ownIt[num % ownIt.length]) {
+  document.writeln("You hypothetically own this car. Isn't that cool?<br>");
+} else {
+  document.writeln("Sadly, you don't own this car. Maybe next time...<br>");
 }
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("Reload this page to get a new car!<br>");
