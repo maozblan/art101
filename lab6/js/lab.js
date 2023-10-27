d6224a48a8061abbf23742a1897f755cf54ffaf1
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-10-27 09:11:16 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,38 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * a couple of variables to print.
+ *
+ * @link   https://maozblan.github.io/art101/lab6/index.html
+ * @file   This file has a bit of variables that print things.
+ * @author Lyssa.
+ * @since  30.10.2023
+ */
+
 
 // Constants
+const transportation = ['car', 'bike', 'legs'];
+const make = ['acura', 'ford', 'toyota', 'bmw', 'tesla'];
+const ran = ['wizards', 'unicorns', 'dragons'];
+const color = ['graphite', 'moonlight', 'sundew', 'mist'];
+const year = [2019, 2021, 1982, 1543, 2026, 1996];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Other Variables
+// random number calcuation from chatGPT: Math.floor(Math.random() * (max - min + 1)) + min;
+let num = Math.floor(Math.random() * (1000 + 1));
+let yourMainRide = {
+  make : make[num % make.length],
+  by : ran[num % ran.length],
+  color : color[num % color.length],
+  year : year[num % year.length],
+  age : function() {
+    return 2023 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Output
+document.writeln("Modes of transportation: " + transportation + "<br>");
+document.writeln("Your Hypothetical Ride: <pre>", JSON.stringify(yourMainRide, null, '\t'), "</pre>");
 
-// let's get this party started
-main();
+// update color of car
+document.getElementById("car_bot").className = yourMainRide.color; 
+
+document.writeln("Reload this page to get a new car!<br>");
