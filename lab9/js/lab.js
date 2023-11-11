93368a4a529d27bdc3b4b599b315f5e6d6e9893f
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-11-11 13:04:32 -0800

    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,8 +1,17 @@
 /**
- * summary.
+ * this is the implmentation for the funky fancy class.
  *
  * @link   https://maozblan.github.io/art101/lab9/index.html
- * @file   about file.
+ * @file   '.special' class implementation.
  * @author Lyssa.
- * @since  00.11.2023
+ * @since  09.11.2023
  */
+
+// add a button to all minor sections and make them clickable
+
+$(".minor-section").each(function() {
+    $(this).append("<button class='toggle-button'>toggle :)</button>");
+});
+$('.toggle-button').click(function() {
+    $(this).parent().toggleClass('toggle');
+});
